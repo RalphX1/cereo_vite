9345a61636ee97e71413ac7179911223b912e1be
Ralph <ralph.emuzel@gmail.com>
2024-12-18 02:52:06 +0100

    coffee

--- src/components/EmailGenerator.jsx
+++ src/components/EmailGenerator.jsx
@@ -22,7 +22,6 @@ const EmailGenerator = () => {
   const copyToClipboard = () => {
     navigator.clipboard.writeText(generatedEmail);
   };
-
   return (
     <div className="max-w-4xl mx-auto p-6 space-y-8">
       <div className="text-center">
